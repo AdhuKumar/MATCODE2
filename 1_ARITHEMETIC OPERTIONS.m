36b1aed8e5e11ce546e29aa04d3a0415957e06f2
ADHU KUMAR <112812597+AdhuKumar@users.noreply.github.com>
2024-05-06 19:39:08 +0530

    Add files via upload

---
+++ 1_ARITHEMETIC OPERTIONS.m
@@ -0,0 +1,21 @@
+clc
+clear all
+clear all
+% Define arrays
+A = [1,2,3,4]
+B = [2,0,1,4]
+
+% Perform element-wise operations
+C = A + B 
+D = A - B 
+E = A .* B
+F = A ./ B
+G = A .\ B
+
+% Apply functions to array A
+H = A .^ 3
+I = A .^ B
+J = exp(A)
+K = log(A)
+L = sin(A)
+M = cosd(A)
